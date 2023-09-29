64661fb70095486ac22b43cb3265ebdb095531a1
guadalupe atim <guadiiatim1309@gmail.com>
2023-09-29 01:31:46 -0300

    metodos de accion

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 using EspacioCadete;
 using EspacioCadeteria;
+using EspacioInforme;
 using EspacioPedido;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,46 +14,64 @@ public class CadeteriaController : ControllerBase
 {
     private Cadeteria? cadeteria;
 
-    [HttpGet]
-    public ActionResult<List<Pedido>> GetPedidos()
-    {
+    private readonly ILogger<CadeteriaController> logger;
 
+    public CadeteriaController(ILogger<CadeteriaController> logger)
+    {
+        this.logger = logger;
+        cadeteria = cadeteria.GetCadeteria();
     }
 
-    [HttpGet]
+    [HttpGet("GetPedidos")]
+    public ActionResult<List<Pedido>> GetPedidos()
+    {
+        var pedidos = cadeteria.GetPedidos();
+        return Ok(pedidos);
+    }
+
+    [HttpGet("GetCadetes")]
     public ActionResult<List<Cadete>> GetCadetes()
     {
-
+        var cadetes = cadeteria.GetCadetes();
+        return Ok(cadetes);
     }
 
     [HttpGet]
     public ActionResult<string> GetInforme()
     {
-
+        var informe = new Informe();
+        return Ok(informe);
     }
 
-    [HttpPost]
-    public ActionResult AgregarPedidos()
+    [HttpPost("AgregarPedido")]
+    public ActionResult AgregarPedidos(Pedido pedidoNuevo)
     {
-
+        cadeteria.AgregarPedido(pedidoNuevo);
+        return Ok(pedidoNuevo);
     }
 
-    [HttpPut]
-    public ActionResult AsignarPedido()
+    [HttpPut("AsignarCadeteAPedido")]
+    public ActionResult AsignarPedido(int idPedido, int idCadete)
     {
-
+        cadeteria.AsignarCadeteAPedido(idPedido, idCadete);
+        var pedidoAsignado = cadeteria.BuscarPedido(idPedido);
+        return Ok(pedidoAsignado);
     }
 
-    [HttpPut]
-    public ActionResult CambiarEstadoPedido()
+    [HttpPut("CambiarEstado")]
+    public ActionResult CambiarEstadoPedido(int idPedido)
     {
-
+        var pedidoBuscado = cadeteria.BuscarPedido(idPedido);
+        pedidoBuscado.CambiarEstado();
+        return Ok(pedidoBuscado);
     }
 
-    [HttpPut]
-    public ActionResult CambiarCadetePedido()
+    [HttpPut("AsignarCadeteAPedido")]
+    public ActionResult CambiarCadetePedido(int idPedido, int idCadete)
     {
-        
+        cadeteria.AsignarCadeteAPedido(idPedido, idCadete);
+        var pedido = cadeteria.BuscarPedido(idPedido);
+        return Ok(pedido);
     }
 
 }
